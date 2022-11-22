c4b3fc2b3ecb9bc2381b743c3afd86e1007058c1
dhapdhap123 <dhapdhap123@naver.com>
2022-11-22 14:39:57 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<LandingPage />} path="/" />
+      <Route element={<LoginPage />} path="/login" />
+      <Route element={<RegisterPage />} path="/register" />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/components/ProfileList.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function ProfileList() {
+  return <div>ProfileList(Landing)</div>;
+  // login <-> non login
+}
+
+export default ProfileList;
