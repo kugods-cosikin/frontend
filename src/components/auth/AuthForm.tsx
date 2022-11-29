d2234704bd68a2f09e68ce2151bec1a2612a699f
dhapdhap123 <dhapdhap123@naver.com>
2022-11-29 23:37:34 +0900

    1129_3

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 /* eslint-disable react/button-has-type */
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { login, register } from '../../lib/api/auth';
 
@@ -9,6 +9,9 @@ interface Props {
 }
 
 function AuthForm({ type }: Props) {
+  const notInitialEmailRender = useRef(false);
+  const notInitialPasswordRender = useRef(false);
+
   const text = type === 'login' ? 'Sign in' : 'Sign up';
 
   const [inputs, setInputs] = useState({
@@ -19,41 +22,59 @@ function AuthForm({ type }: Props) {
   const { email, password, passwordConfirm } = inputs;
 
   const [passwordView, setPasswordView] = useState(false);
-  const [emailValid, setEmailValid] = useState(true);
-  const [passwordValid, setPasswordValid] = useState({
-    format: true,
-    confirm: true,
+  const [emailValid, setEmailValid] = useState({
+    format: false,
     message: '',
-    first: true,
+  });
+  const [passwordValid, setPasswordValid] = useState({
+    format: false,
+    confirm: false,
+    message: '',
   });
 
-  const validateInputs = (email: string, password: string, passwordConfirm: string) => {
+  const validateEmail = (email: string) => {
     const regExpEmail = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    const eValid = regExpEmail.test(email);
-    setEmailValid(eValid);
-
+    const format = regExpEmail.test(email);
+    if (format === true) {
+      setEmailValid({ format: true, message: '' });
+    } else {
+      setEmailValid({ format: false, message: '이메일이 유효하지 않습니다' });
+    }
+  };
+  const validatePassword = (password: string, passwordConfirm: string) => {
     const regExpPassword = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{6,16}$/;
     const format = regExpPassword.test(password);
-    const confirm = password === passwordConfirm;
+    setPasswordValid({ ...passwordValid, format });
 
-    setPasswordValid({ ...passwordValid, first: false });
+    const confirm = password === passwordConfirm;
+    setPasswordValid({ ...passwordValid, confirm });
+
     switch (true) {
-      case password === passwordConfirm && format:
-        setPasswordValid({ ...passwordValid, format, confirm, message: '' });
-        break;
+      case format === true && confirm === true:
+        setPasswordValid({ format, confirm, message: '' });
+        return;
       case format === true && confirm === false:
-        setPasswordValid({ ...passwordValid, format, confirm, message: '비밀번호가 일치하지 않습니다.' });
-        break;
+        setPasswordValid({ format, confirm, message: '비밀번호가 일치하지 않습니다.' });
+        return;
       case format === false && confirm === true:
-        setPasswordValid({ ...passwordValid, format, confirm, message: '비밀번호가 유효하지 않습니다.' });
-        break;
+        setPasswordValid({
+          format,
+          confirm,
+          message: '비밀번호가 유효하지 않습니다. (특수문자, 영문, 숫자 포함 6-16자)',
+        });
+        return;
       case format === false && confirm === false:
-        setPasswordValid({ ...passwordValid, format, confirm, message: '비밀번호가 유효하지 않습니다.' });
+        setPasswordValid({
+          format,
+          confirm,
+          message: '비밀번호가 유효하지 않습니다. (특수문자, 영문, 숫자 포함 6-16자)',
+        });
         break;
       default:
         break;
     }
   };
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setInputs({
@@ -67,21 +88,36 @@ function AuthForm({ type }: Props) {
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    validateInputs(email, password, passwordConfirm);
-
     if (type === 'login') {
-      if (emailValid && !passwordValid.first) {
+      if (emailValid && password) {
         console.log('login');
         // login({ email, password });
       }
     }
     if (type === 'register') {
-      if ((emailValid && passwordValid.format && passwordValid.confirm, !passwordValid.first)) {
+      if (emailValid && passwordValid.format && passwordValid.confirm) {
         console.log('register');
         // register({ email, password });
       }
     }
   };
+
+  useEffect(() => {
+    if (notInitialEmailRender.current) {
+      validateEmail(email);
+    } else {
+      notInitialEmailRender.current = true;
+    }
+  }, [email]);
+  useEffect(() => {
+    if (type === 'register') {
+      if (notInitialPasswordRender.current) {
+        validatePassword(password, passwordConfirm);
+      } else {
+        notInitialPasswordRender.current = true;
+      }
+    }
+  }, [password, passwordConfirm]);
   return (
     <>
       <div>
@@ -91,11 +127,11 @@ function AuthForm({ type }: Props) {
       <h1>{text}</h1>
       <form onSubmit={onSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
         <input type="text" name="email" placeholder="Email" onChange={onChange} value={email} />
-        {emailValid === false ? '이메일 양식이 올바르지 않습니다.' : null}
+        {emailValid.message ? emailValid.message : null}
         <input
           type={passwordView ? 'text' : 'password'}
           name="password"
-          placeholder={type === 'register' ? 'Password (특수문자, 영문, 숫자 포함 6-16자)' : 'Password'}
+          placeholder={type === 'register' ? 'Password' : 'Password'}
           onChange={onChange}
           value={password}
         />
